9d2cdc610b3d27c91f82b5abec3256b0edacebd6
BHVD <mauhieu198@gmail.com>
2025-04-05 19:36:44 +0700

    change somthing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+const port = process.env.PORT || 4000;
 app.use(express.json());
 
 
@@ -39,4 +39,4 @@ app.post('/verify-unity-token', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(port, () => console.log('Server listening on port', port));
