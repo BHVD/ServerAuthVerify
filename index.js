af41adc7cd2c8ebd41d268f05f4f9206ce19f2e9
BHVD <mauhieu198@gmail.com>
2025-04-24 17:37:41 +0700

    verufy get player avatar, level

--- index.js
+++ index.js
@@ -25,17 +25,46 @@ app.get('/verify-unity-token', async (req, res) => {
 
         const PlayerName = response.data.name; // sub là User ID trong Unity
         const nickname = PlayerName; // Tùy bạn đặt logic
-
-        // Trả kết quả cho Photon
-        return res.json({
-            ResultCode: 1,
-            UserId: playerId,
-            NickName: nickname,
-            Data: {
-                skin: "blue",
-                level: 5
+        var listKey = "keys=PLAYER_LEVEL&keys=PLAYER_AVATAR"
+        try {
+            const responseInfo = await axios.get(
+                `https://cloud-save.services.api.unity.com/v1/data/projects/${process.env.PROJECT_ID}/players/${playerId}/public/items?${listKey}`,
+                {
+                    headers: {
+                        Authorization: accessToken,
+                        contentType: 'application/json'
+                    }
+                }
+            )
+            const ReturnData = {
+                level: 1,
+                AvatarId: "#0"
             }
-        });
+
+            if (responseInfo.data.results) {
+                responseInfo.data.results.forEach(item => {
+                    if (item.key === "PLAYER_LEVEL") {
+                        ReturnData.level = item.value;
+                    } else if (item.key === "PLAYER_AVATAR") {
+                        ReturnData.AvatarId = item.value
+                    }
+                });
+            }
+
+
+            // Trả kết quả cho Photon
+            return res.json({
+                ResultCode: 1,
+                UserId: playerId,
+                NickName: nickname,
+                Data: ReturnData
+            });
+
+
+
+        } catch (err) {
+            res.status(err).json({ ResultCode: 0, Message: "Cannot get player info" })
+        }
 
     } catch (err) {
         return res.status(401).json({ ResultCode: 0, Message: "Invalid or expired token" });
@@ -131,12 +160,12 @@ app.post('/reward', async (req, res) => {
                 }
             });
         }
-        if(!instance.MatchInfo){
-            return res.status(400).json({ResultCode:0 , Message: "Player don't have Match"})
+        if (!instance.MatchInfo) {
+            return res.status(400).json({ ResultCode: 0, Message: "Player don't have Match" })
         }
         const secret = await signWithHMAC(`${instance.MatchInfo.matchId}${instance.MatchInfo.startTimestamp}`, process.env.SECRET_KEY);
 
-        const results ={}// bindding///////////////
+        const results = {}// bindding///////////////
 
         var message = ""
         for (let index = 0; index < Rewrads.Currencies.length; index++) {
@@ -156,14 +185,14 @@ app.post('/reward', async (req, res) => {
         if (!isValid) {
             return res.status(400).json({ ResultCode: 0, Message: "Invalid token reward" })
         }
-        
+
         console.log("xac thuc thanh cong(7)")
 
         //update XP
         if (Rewrads.XP) {
             instance.CurrentXp += Rewrads.XP
             var playerXPNeededKey = `LEVEL_UP_NEEDED_${instance.level}`
-            
+
             try {
                 const response = await axios.get(
                     `https://config.unity3d.com/api/v1/settings?projectId=${process.env.PROJECT_ID}&key=${playerXPNeededKey}`,
@@ -178,37 +207,37 @@ app.post('/reward', async (req, res) => {
                     response.data.configs.settings &&
                     response.data.configs.settings[playerXPNeededKey]
                 ) {
-                    instance.xpNeedLevelUp =  response.data.configs.settings[playerXPNeededKey]
+                    instance.xpNeedLevelUp = response.data.configs.settings[playerXPNeededKey]
                 }
-                console.log('xp'+instance.xpNeedLevelUp)
+                console.log('xp' + instance.xpNeedLevelUp)
 
             } catch (error) {
                 console.log(error)
-                return res.status(error.status).json({ResultCode: 0, Message:"Fail to get level up xp needed!"})
+                return res.status(error.status).json({ ResultCode: 0, Message: "Fail to get level up xp needed!" })
             }
 
             const shouldPlayerLevelUp = instance.CurrentXp >= instance.xpNeedLevelUp
 
             const Data = []
 
-            if(shouldPlayerLevelUp){
+            if (shouldPlayerLevelUp) {
                 instance.level++
                 instance.CurrentXp = instance.CurrentXp - instance.xpNeedLevelUp
                 // add to reward
-                const levelUpReward = {currencyId:"COIN", amount:999}
+                const levelUpReward = { currencyId: "COIN", amount: 999 }
                 Rewrads.Currencies.push(levelUpReward)
                 results.isLevelUp = shouldPlayerLevelUp
                 results.levelUpReward = levelUpReward
-                Data.push({key:"PLAYER_LEVEL", value:instance.level})
+                Data.push({ key: "PLAYER_LEVEL", value: instance.level })
             }
-            Data.push({key:"PLAYER_XP", value: instance.CurrentXp})
+            Data.push({ key: "PLAYER_XP", value: instance.CurrentXp })
 
             //update player data
             try {
                 //https://cloud-save.services.api.unity.com/v1/data/projects/{projectId}/players/{playerId}/public/item-batch
                 const response = await axios.post(
                     `https://cloud-save.services.api.unity.com/v1/data/projects/${process.env.PROJECT_ID}/players/${playerId}/public/item-batch`,
-                    {data:Data},
+                    { data: Data },
                     {
                         headers: {
                             Authorization: accessToken,
@@ -242,7 +271,7 @@ app.post('/reward', async (req, res) => {
                         }
                     }
                 )
-                return {id: reward.currencyId, amount: Data.amount, succeed: true}
+                return { id: reward.currencyId, amount: Data.amount, succeed: true }
 
             } catch (error) {
                 return { id: reward.currencyId, amount: reward.amount, succeed: false };
